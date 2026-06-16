9202d0c4d22a675cf60585e2cd32ce95f0817ec1
MrFerr3t <158501116+MrFerr3t@users.noreply.github.com>
2026-06-16 17:01:16 +0000

    final v1

---
+++ intro data science/main.py
@@ -0,0 +1,24 @@
+import data
+import clustering
+import numpy as np
+
+print("Part A:  ")
+df = data.load_data("hw2/london_sample_500.csv")
+df = data.add_new_columns(df)
+data.data_analysis(df)
+
+print("\nPart B: ")
+features = ['hum', 'wind_speed']
+transformed_data = clustering.transform_data(df, features)
+
+for i, k in enumerate([3, 4, 6]):
+    labels, centroids = clustering.kmeans(transformed_data, k)
+
+    print(f"k = {k}")
+    print(np.array_str(centroids, precision=3, suppress_small=True))
+    if i < 2:
+        print()
+
+
+    plot_path = f"hw2/plot_{k}.png"
+    clustering.visualize_results(transformed_data, labels, centroids, plot_path)
